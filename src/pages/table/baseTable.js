50abadf51e3cb3871c2192ed5f158225a8ee533d
曾志勇 <zengzhiyong@jiaxincloud.com>
2018-12-24 18:05:52 +0800

    loading && 数据拦截渲染

--- src/pages/table/baseTable.js
+++ src/pages/table/baseTable.js
@@ -14,6 +14,7 @@ export default class BaseTable extends React.Component {
       status: '工作',
       address: '西湖区湖底公园1号',
       interest: '篮球',
+      sex: '1'
     }, {
       id: '2',
       name: '胡彦祖',
@@ -21,6 +22,7 @@ export default class BaseTable extends React.Component {
       status: '娱乐',
       address: '西湖区湖底公园1号',
       interest: '足球',
+      sex: '1'
     }];
 
     this.setState({
@@ -35,7 +37,8 @@ export default class BaseTable extends React.Component {
       url: '/table/list',
       params:{
         page: 1
-      }
+      },
+      isShowLoading: true,
     }).then((data) => {
       this.setState({
         dataSource2: data.list
@@ -56,6 +59,12 @@ export default class BaseTable extends React.Component {
       {
         title: '年龄',
         dataIndex: 'age'
+      },{
+        title: '性别',
+        dataIndex: 'sex',
+        render(sex){
+          return sex == 1 ? '男' : '女'
+        }
       },
       {
         title: '状态',
@@ -63,7 +72,20 @@ export default class BaseTable extends React.Component {
       },
       {
         title: '爱好',
-        dataIndex: 'interest'
+        dataIndex: 'interest',
+        render(interset) {
+          let config = {
+            '1': '游泳',
+            '2': '足球',
+            '3': '写作',
+            '4': '打码',
+            '5': '吃吃吃',
+            '6': '看剧',
+            '7': '画画',
+            '8': '跳舞',
+          }
+          return config[interset]
+        }
       },
       {
         title: '地址',
